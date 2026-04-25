2c8ec0e5fa378b15684c7523162d7f86d84dac54
Grezia <a01713876@tec.mx>
2026-04-25 16:59:00 -0600

    Moving from a static project into a dynamic structure, following the MVC structure.

---
+++ index.js
@@ -0,0 +1,68 @@
+const http    = require('http');
+const express = require('express');
+const path    = require('path');
+const fs      = require('fs');
+const app     = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send("Hola Mundo");
+    res.end(); 
+});
+
+app.get('/dashboard', (req, res, next) => {
+
+    res.render('index');
+    }); 
+
+app.get('/clientes', (req, res, next) => {
+
+    res.render('clientes');
+    });
+
+app.get('/contratos', (req, res, next) => {
+
+    res.render('contratos');
+    });
+
+app.get('/expedientes', (req, res, next) => {
+
+    res.render('expedientes');
+    });
+
+app.get('/listas', (req, res, next) => {
+
+    res.render('listas');
+    });
+
+app.get('/reportes', (req, res, next) => {
+
+    res.render('reportes');
+    });
+
+/*app.get('/login', (req, res, next) => {
+
+    res.render('login');
+    });
+
+app.get('/canal-interno', (req, res, next) => {
+
+    res.render('canal-interno');
+    });
+
+app.get('/kyc', (req, res, next) => {
+
+    res.render('kyc');
+    });*/
+
+const server = http.createServer( (req, res) => {    
+    console.log(req.url);
+});
+app.listen(3001);
