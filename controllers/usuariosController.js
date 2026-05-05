cf856a89101fe6c8e094e5feff74fe4d27e8d228
Grezia <a01713876@tec.mx>
2026-05-05 15:55:37 -0600

    Fleshed out the model and controller files for login page. Worked on a test version of login page just to see it rendered

---
+++ controllers/usuariosController.js
@@ -0,0 +1,55 @@
+const model = require("../models/usuariosModel.js")
+const bcrypt = require('bcrypt');
+
+module.exports.render_login = async(req,res) =>{
+    res.render("usuarios/login", { registro: false });
+}
+
+module.exports.do_login = async(req,res) =>{
+        try {
+        const usuario = await model.User.findByEmail(req.body.email);
+        //checking for existing user
+        if (!email) {
+            return res.redirect('/usuarios/login');
+        }
+
+        //verify password
+        const doMatch = await bcrypt.compare(req.body.password, usuario.password);
+        if (!doMatch) {
+            return res.redirect('/usuarios/login');
+        }
+
+        //load user permissions
+        const permisos = await model.User.getPermisos(usuario.email);
+        req.session.email = usuario.email;
+        req.session.rol = usuario.rol;
+        req.session.isLoggedIn = true;
+        req.session.permisos   = permisos; 
+
+        //redirection based on role
+        switch (usuario.rol) {
+            case 'admin':
+                return res.redirect ('/admin/dashboard');
+
+            case 'oficial':
+                return res.redirect ('/oficial/dashboard');
+
+            case 'operador':
+                return res.redirect ('/operador/dashboard');
+
+            case 'cliente':
+                return res.redirect ('/forms/kyc-form');
+        }
+
+    } catch (e) {
+        console.error(e);
+        res.redirect('/usuarios/login');
+    }
+};
+
+exports.get_logged = async (req, res) => {
+    const email = await model.User.findByEmail(req.session.email);
+    if (!email) return res.redirect('/usuarios/login');
+    res.render('usuarios/logged', { user: email });
+};
+
