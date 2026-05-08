b90ff37d70227bec57e05075fc69d46b6949861b
Grezia <a01713876@tec.mx>
2026-05-07 19:42:28 -0600

    Fixing operador route with main page establishment.

--- controllers/operadorController.js
+++ controllers/operadorController.js
@@ -1,7 +1,20 @@
 exports.operador_index = (req, res) => {
-    res.render('operador/dashboard', {
+    res.render('/operador/dashboard', {
                 pageTitle: 'Dashboard Principal',
                 buttonText: '+ Nuevo cliente',
                 buttonLink: '/kyc'
             });
 };
+
+exports.operador_clientes = (req, res) => {
+    res.render('operador/clientes', {
+                pageTitle: 'Clientes'
+            });
+};
+
+exports.operador_expedientes = (req, res) => {
+    res.render('operador/expedientes', {
+                pageTitle: 'Expedientes de cliente'
+            });
+};
+
