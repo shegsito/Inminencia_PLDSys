5ab22ef011a56c53dc3f8a8c8e0d6d0ccf8e2df6
Grezia <a01713876@tec.mx>
2026-05-13 13:28:42 -0600

    add test file

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/dashboard',     require('./dashboard'));
-router.use('/clientes',      require('./clientes'));
-router.use('/contratos',     require('./contratos'));
-router.use('/expedientes',   require('./expedientes'));
-router.use('/listas',        require('./listas'));
-router.use('/reportes',      require('./reportes'));
-router.use('/canal-interno', require('./canalInterno'));
-router.use('/alertas',       require('./alertas'));
 router.use('/api/clientes',   require('./api/clientes'));
 router.use('/api/expediente', require('./api/expedientes'));
 router.use('/dashboard', require('./dashboardRoutes'));
