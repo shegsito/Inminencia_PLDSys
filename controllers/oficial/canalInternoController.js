c8e47cc9bcc85ef88d02900e58b669032934c0a0
Grezia <a01713876@tec.mx>
2026-05-26 12:13:16 -0600

    Added consulting evidence to bitacora

--- controllers/oficial/canalInternoController.js
+++ controllers/oficial/canalInternoController.js
@@ -69,6 +69,16 @@ exports.verEvidencia = async (req, res) => {
         if (!fs.existsSync(findPath)){
             return res.status(404).send('Archivo no encontrado en el servidor')};
 
+        //audit log captures this activity
+        const idusuario = req.session.idusuario;
+        const ipusuario = req.ip;
+
+        const bitacora = `INSERT INTO bitacora (idusuario, accion, entidad_afect, id_entidad, ip_origen, fecha)
+                          VALUES ($1, $2, $3, $4, $5, NOW())`
+                          ;
+
+        await pool.query(bitacora, [idusuario, 'CONSULTAR EVIDENCIA', 'reporte interno', id, ipusuario]);
+
         //formatting since this table does not have format column
         const format = path.extname(findPath).toLowerCase();
         let contentType = 'application/octet-stream';
