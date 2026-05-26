7a26a241dbbab308a8b964a70d1abc1f1f994f32
Grezia <a01713876@tec.mx>
2026-05-26 11:03:24 -0600

    creating internal report added to bitacora

--- models/canalInternoModel.js
+++ models/canalInternoModel.js
@@ -18,13 +18,44 @@ exports.fetchAll = async () => {
 };
 
 //new internal report
-exports.reporteInterno = async (queja_desc, evidenica, fecha_int, idasignadoa) => {
-    const sql = `INSERT INTO reporte_interno (descripcion, ruta_evidencia, estatus, fecha, idasignadoa, anonimo)
+exports.reporteInterno = async (queja_desc, evidencia, fecha_int, idasignadoa, idusuario, ipOrigin) => {
+    const dbClient = await pool.connect();
+    try {
+        //tracked transaction
+        await dbClient.query('BEGIN');
+
+        //create internal report
+        const sql = `INSERT INTO reporte_interno (descripcion, ruta_evidencia, estatus, fecha, idasignadoa, anonimo)
                  VALUES ($1, $2, 'recibido', $3, $4, TRUE)
                  RETURNING *`
                  ;
-    const res = await pool.query(sql, [queja_desc, evidenica, fecha_int, idasignadoa]);
-    return res.rows [0];
+
+        const res = await dbClient.query(sql, [queja_desc, evidencia, fecha_int, idasignadoa]);
+        const reporte_int = res.rows [0];
+
+        //extract internal report identifier
+        const idreporteInterno = reporte_int.idreporteint;
+
+        //audit log register
+        const bitacora = `INSERT INTO bitacora (idusuario, accion, entidad_afect, id_entidad, ip_origen, fecha)
+             VALUES ($1, $2, $3, $4, $5, NOW())`
+
+        await dbClient.query(bitacora, [idusuario, 'CREAR REPORTE INTERNO', 'reporte interno', reporte_int, ipOrigin]);
+
+        //DB change if all succeeds
+        await dbClient.query('COMMIT');
+
+        return reporte_int;
+        
+    } catch(e) {
+        //any failures do not affect DB
+        await dbClient.query('ROLLBACK');
+        console.error('Error en transacción: ', e);
+        throw e;
+    } finally {
+        //evades saturation of DB conneections
+        dbClient.release();
+    }
 };
 
 //fetch case status for operador
