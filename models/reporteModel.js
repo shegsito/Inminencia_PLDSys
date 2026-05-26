7f72103455108a208e4d21a7a6c197f863901c9d
Grezia <a01713876@tec.mx>
2026-05-26 17:45:07 -0600

    front end for reportes page

--- models/reporteModel.js
+++ models/reporteModel.js
@@ -1,5 +1,12 @@
 const pool = require('../config/db');
 
-const ReporteModel = {};
-
-module.exports = ReporteModel;
+//fetch records
+exports.fetchAll = async () => {
+    const sql = `
+        SELECT generado_en, formato, estatus, ruta_archivo
+        FROM reporte_regulatorio
+        ORDER BY generado_en DESC
+    `;
+    const { rows } = await pool.query(sql);
+    return rows;
+};
--- routes/oficial/index.js
+++ routes/oficial/index.js
@@ -42,6 +42,7 @@ router.get(`/alertas/api/alertasData`, alertas.getAlertasData);
 router.get('/contratos/contratosCount', contratos.count);
 router.get('/contratos/contratosData', contratos.contratos);
 router.get('/canalInterno/canalInternoData', canalInterno.getCanalInternoData);
+router.get('/reportes/reportesData', reportes.getReportesData);
 
 router.get('/clientes/:id',    clientes.getCliente);
 router.put('/clientes/:id',    clientes.actualizarCliente);
