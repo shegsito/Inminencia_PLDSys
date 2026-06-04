36da871cb0bd37a3cfcc8a6109a2467b19ce9eb5
gr3z14 <a01713876@tec.mx>
2026-06-03 21:01:09 -0600

    Delete routes directory

--- routes/api/clientes.js
+++
@@ -1,7 +0,0 @@
-const express    = require('express');
-const router     = express.Router();
-const controller = require('../../controllers/api/clientesController');
-
-router.post('/', controller.registrar);
-
-module.exports = router;
--- routes/cliente/index.js
+++
@@ -1,7 +0,0 @@
-const express    = require('express');
-const router     = express.Router();
-const registro   = require('../../controllers/cliente/registroController');
-
-router.get('/', registro.index);
-
-module.exports = router;
